bec4d24fc917fd9e2d62a072b6ef74b676e21d7f
Edrard A <demonand@gmail.com>
2023-09-15 14:05:55 +0300

    feat: Round added, by default its 1

--- src/Log/Timer.php
+++ src/Log/Timer.php
@@ -12,15 +12,19 @@ class Timer
     {
         static::$time_start[$type] = microtime(true);
     }
-    public static function endTime($type = 'global')
+    public static function endTime($type = 'global', $round = 1)
     {
         static::$time_end[$type] = microtime(true);
         static::$execution_time[$type] = static::$time_end[$type] - static::$time_start[$type];
+        if($round != 0){
+            $koeff = pow(10, $round);
+            static::$execution_time[$type] = round(static::$execution_time[$type] * $koeff)/$koeff;
+        }
     }
-    public static function getTime($type = 'global')
+    public static function getTime($type = 'global', $round = 1)
     {
        if(!isset(static::$execution_time[$type]) ||  !static::$execution_time[$type]){
-            static::endTime($type);
+            static::endTime($type,$round);
         }
         return isset(static::$execution_time[$type]) ? static::$execution_time[$type] : "" ;
     }
